8bf49e70514a422875deec99fd90a79dbbd12baa
t0n1-252 <antoni@duobro.com.br>
2025-09-04 11:34:35 -0300

    payload cms integration

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <Script
         id="google-tag-manager"
         strategy="afterInteractive"
