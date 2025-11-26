b65afe50b6591b017b22ac03b66631d0a1d5965c
t0n1-252 <antoni@duobro.com.br>
2025-11-26 16:09:42 -0300

    feat: integrate Google Analytics for enhanced tracking and analytics

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -44,6 +44,22 @@ export default function RootLayout({
           `,
         }}
       />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-GJS1HH2Y6D"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GJS1HH2Y6D');
+          `,
+        }}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
